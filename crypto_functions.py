1e1361c40b3a73e4467915cff811afc3665df2b2
benghaun <ang.benghaun@gmail.com>
2018-04-01 22:46:07 +0800

    removed some debugging print statements

--- crypto_functions.py
+++ crypto_functions.py
@@ -127,14 +127,11 @@ def encrypt_request(req, pub_key):
 
 #verifies if a signature was created from a given raw data using the correct key
 def verify_signature(raw, signature, public_key):
-    print(raw)
-    print(public_key)
-    print(signature)
     hash_object = SHA256.new(data = bytes(raw, encoding = "utf-8"))
-    print(hash_object)
     publicKey = RSA.import_key(public_key)
     try:
         pkcs1_15.new(publicKey).verify(hash_object, signature)
         return True
-    except:
+    except Exception as e:
+        print(e.message)
         return False

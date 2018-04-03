a70f2e4127a38b2dc09fe3aa57b3e8add2f907e5
nightzsky <33797097+nightzsky@users.noreply.github.com>
2018-04-03 09:23:12 +0800

    added check access

--- companybackendserver.py
+++ companybackendserver.py
@@ -92,7 +92,7 @@ def check_if_user_info_exists(user_info):
     return exist
 
 #check if the user exists on the database
-def check_for_login(username,password,encrypted_merkle_raw):
+def check_for_login(username,password,encrypted_merkle_raw,block_id):
     conn,cur,rows = select_db("*","COMPANY_DATABASE")
     can_login = False
     for row in rows:
@@ -326,6 +326,15 @@ def register_user():
     block_id = decrypted["block_id"]
     AES_key = decrypted["AES_key"]
     
+#    #post request to kyc backend to retrieve user block of info
+#    r = requests.post("https://kyc-project.herokuapp.com/register_org", json = {"block_id":block_id})
+#    data_received = json.loads(r.text)
+#    can_access = data_received["access"]
+#    if not can_access:
+#        resp = Response(json.dumps({"Error":"This user is disabled"}))
+#        resp.status_code = 400
+#        return resp
+    
     if (isValidUsername(username) == False):
         resp = Response(json.dumps({"Error":"Username contains invalid characters"}))
         resp.status_code = 400
@@ -351,26 +360,33 @@ def register_user():
     if (r.status_code == 200): #if success in retrieving user info from kyc
         # received ENCRYPTED user data from kyc backend
         user_data = json.loads(r.text)
-        print(type(user_data))
-        user_data = user_data["userData"]
-        del user_data["$class"]
-        print(user_data)
+        can_access = user_data["access"]
+        # check if the user got report lost of token
+        if (can_access):      
+            print(type(user_data))
+            user_data = user_data["userData"]
+            del user_data["$class"]
+            print(user_data)
            
-        #decrpyt the user data with AES key
-        for key in user_data:
-            print("decrypting %s now"%key)
-            user_data[key] = aes_decrypt(user_data[key],AES_key)
+            #decrpyt the user data with AES key
+            for key in user_data:
+                print("decrypting %s now"%key)
+                user_data[key] = aes_decrypt(user_data[key],AES_key)
             
-        print(user_data)
-            
-        if(check_if_user_info_exists(user_data) == True):
-            resp = Response(json.dumps({"Error":"You have already registered with this company!"}))
-            resp.status_code = 409
+            print(user_data)
+                
+            if(check_if_user_info_exists(user_data) == True):
+                resp = Response(json.dumps({"Error":"You have already registered with this company!"}))
+                resp.status_code = 409
+                return resp
+            else:    
+                #post the user data to the company database along with password and username
+                add_user_to_database(username,password,user_data)
+                return "Done"
+        else: 
+            resp = Response(json.dumps({"Error":"The user is disabled."}))
+            resp.status_code = 400
             return resp
-        else:    
-            #post the user data to the company database along with password and username
-            add_user_to_database(username,password,user_data)
-            return "Done"
         
     else: #if fail to retrieve the user info
         resp = Response(json.dumps(json.loads(r.text)))
@@ -398,11 +414,22 @@ def login_org():
     
     username = decrypted["username"]
     password = decrypted["password"]
+    block_id = decrypted["block_id"]
     encrypted_merkle_raw = decrypted["merkle_raw"]
     
     print("username %s"%username)
     print("password %s"%password)
+    print("block_id %s"%block_id)
     print("merkle_raw %s"%encrypted_merkle_raw)
+    
+    #    #post request to kyc backend to retrieve user block of info
+#    r = requests.post("https://kyc-project.herokuapp.com/register_org", json = {"block_id":block_id})
+#    data_received = json.loads(r.text)
+#    can_access = data_received["access"]
+#    if not can_access:
+#        resp = Response(json.dumps({"Error":"This user is disabled"}))
+#        resp.status_code = 400
+#        return resp
     
     can_login = check_for_login(username,password,bytes(java_to_python_bytes(encrypted_merkle_raw)))
     
--- crypto_functions.py
+++ crypto_functions.py
@@ -127,6 +127,8 @@ def encrypt_request(req, pub_key):
 #verifies if a signature was created from a given raw data using the correct key
 def verify_signature(raw, signature, public_key):
     hash_object = SHA256.new(data = bytes(raw, encoding = "utf-8"))
+#    print(type(raw))
+#    print(type(signature))
     print(hash_object)
     publicKey = RSA.import_key(public_key)
     try:
